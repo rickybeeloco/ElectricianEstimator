5f749c06514633097a9a6481ea7910072fc8c0c0
rickybeeloco <reekardoe@yahoo.com>
2020-03-16 13:18:28 -0700

    Updates

---
+++ precache-manifest.87861b20654db91f0dd4b62ab0b0ae1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edeb946dd2b9bead31af90907eff6102",
+    "url": "/ElectricianEstimator/index.html"
+  },
+  {
+    "revision": "5c4ac02366e9c13b6480",
+    "url": "/ElectricianEstimator/static/css/main.6d555915.chunk.css"
+  },
+  {
+    "revision": "61b2f3f8971355fa898e",
+    "url": "/ElectricianEstimator/static/js/2.c367cba6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ElectricianEstimator/static/js/2.c367cba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c4ac02366e9c13b6480",
+    "url": "/ElectricianEstimator/static/js/main.792fa5b3.chunk.js"
+  },
+  {
+    "revision": "9b0c4bcbac1f803d65fb",
+    "url": "/ElectricianEstimator/static/js/runtime-main.22c3805d.js"
+  }
+]);
